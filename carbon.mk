6d5c90e7b4a722e623f9ced806ebc19d4f6b08d3
winner00 <TOverturf@gmail.com>
2013-04-24 16:00:04 -0600

    Add PA overlay.

--- carbon.mk
+++ carbon.mk
@@ -18,6 +18,9 @@
 # name
 PRODUCT_RELEASE_NAME := Nexus7-GSM
 
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_tvdpi
+
 # device
 $(call inherit-product, device/asus/tilapia/full_tilapia.mk)
 
